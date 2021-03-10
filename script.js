065fd9e9423b46d35e1a3d024e4586139cba496a
Thai Bang <thaibang1983hn@gmail.com>
2021-03-10 11:00:36 +0100

    The new index HTML file

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.getElementById("checkGuess").onclick = function () {
+  var randonmNumber = Math.random();
 
+  randonmNumber = randonmNumber * 6;
+
+  randonmNumber = Math.floor(randonmNumber);
+
+  if (document.getElementById("guess").value == randonmNumber) {
+    alert(" Well done! You got it!!!");
+  } else {
+    alert("Nope! The number was" + randonmNumber);
+  }
+};
